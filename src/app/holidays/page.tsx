fc0ad0a08a929d7e37601085a6e42be88c0bf838
Adventus-Advenit <studiofilmowepiwnica@gmail.com>
2026-06-10 17:38:19 +0200

    Dodanie bazy danych

---
+++ src/app/holidays/page.tsx
@@ -0,0 +1,7 @@
+export default function HolidaysPage() {
+  return (
+    <div>
+      <h1>Urlopy</h1>
+    </div>
+  );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Logowanie</h1>
+    </div>
+  );
+}
